123e98e5d5529b43ec15a0f993fd1285b6f35dd6
Juss <juss-joosep.holdt@voco.ee>
2025-04-01 09:00:15 +0300

    YL 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Meals from "./components/Meals";
+
+const App = () => {
+ return (
+    <>
+     <Header/>
+     <Meals/>
+    </>
+  );
+}
+
+export default App;
